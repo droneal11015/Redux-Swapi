1644bfb00b77fb4a265d240a0d0ca6093cd28292
droneal11015 <droneal11015@gmail.com>
2019-05-22 17:22:31 -0400

    added applyMiddleware to index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ import rootReducer from "./reducers";
 // rootReducer from ./reducers
 
 const store = createStore(
-  rootReducer
+  rootReducer, 
+  applyMiddleware(thunk, logger)
   /* applyMiddleware goes here */
 );
 
